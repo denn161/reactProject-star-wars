d4a73ba7e859035a62e65a1bc06846b2a7f6b486
denn161 <denn161@inbox.ru>
2021-10-07 01:43:37 +0300

    update project

---
+++ src/constants/repo.js
@@ -0,0 +1,1 @@
+export const REPO_NAME = 'reactProject-star-wars'
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,13 +1,14 @@
 
 import { Route, BrowserRouter,Switch } from 'react-router-dom';
 import routesConfig from '@routes/routesConfig';
+import { REPO_NAME } from '@constants/repo';
 import Header from '@components/Header';
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <div className={styles.wrapper}>
         <Header/>
        
